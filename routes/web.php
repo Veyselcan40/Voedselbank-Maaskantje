211fc9fdb13feed49d06439d0fc244171547d2a4
Emin <337858@student.mboutrecht.nl>
2025-06-27 16:51:44 +0200

    inital commi

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\VoedselpakketController;
 
-Route::resource('voedselpakketten', VoedselpakketController::class);
 
 
 Route::get('/', function () {
